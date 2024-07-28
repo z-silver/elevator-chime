195fc0de5477ee82622d21dbf9c4b86816d02d0d
Marinho <marinho.pinotti@gmail.com>
2024-07-28 17:46:40 -0300

    Fix string scrambling

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 const VM = @import("VM.zig");
 
 pub fn main() !void {
-    const buffer_init: [32]u8 = "000000000000000000000000000000\x00\x00".*;
+    const buffer_init: [32]u8 = "00000000000000000000000000000\n\x00\x00".*;
+    std.debug.print("buffer: {s}", .{&buffer_init});
     var echo_image = [_]i32{
         VM.Code.from_slice(&.{
             .literal,
@@ -27,7 +28,7 @@ pub fn main() !void {
         30,
     } ++ @as([8]i32, @bitCast(buffer_init));
 
-    VM.image_native_to_big(&echo_image);
+    VM.image_native_to_big(echo_image[0 .. echo_image.len - 8]);
     var vm_storage = VM{ .ram = &echo_image };
     const vm = &vm_storage;
 
