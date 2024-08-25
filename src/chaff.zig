516b56ae36968cd60a410082bcfe0a13e0cb2e66
Marinho <marinho.pinotti@gmail.com>
2024-08-25 00:04:51 -0300

    Tidy up parser parameters and fix compilation

--- src/chaff.zig
+++ src/chaff.zig
@@ -2,14 +2,118 @@ const std = @import("std");
 const data = @import("data.zig");
 const assert = std.debug.assert;
 
-const Chaff = @This();
-
-allocator: std.mem.Allocator,
-dialect: Dialect = lemos_dialect,
-
 pub const Dialect = std.StaticStringMap(data.Op);
 pub const Environment = std.StringHashMap(i32);
 const Memory = std.ArrayList(i32);
+
+pub fn parse(
+    allocator: std.mem.Allocator,
+    dialect: Dialect,
+    source: []const u8,
+    error_position: *u32,
+) ![]i32 {
+    var parser = try Parser.init(
+        allocator,
+        dialect,
+        source,
+        error_position,
+    );
+    defer parser.deinit();
+    return parser.run();
+}
+
+const Parser = struct {
+    allocator: std.mem.Allocator,
+    dialect: Dialect,
+    source: []const u8,
+    error_position: *u32,
+    labels: Environment,
+    constants: Environment,
+    memory: Memory,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        dialect: Dialect,
+        source: []const u8,
+        error_position: *u32,
+    ) !Parser {
+        assert(source.len < std.math.maxInt(u32) + 2);
+        var memory = Memory.init(allocator);
+        errdefer memory.deinit();
+
+        var labels = Environment.init(allocator);
+        errdefer labels.deinit();
+
+        var constants = Environment.init(allocator);
+        errdefer constants.deinit();
+
+        return .{
+            .allocator = allocator,
+            .dialect = dialect,
+            .memory = memory,
+            .labels = labels,
+            .constants = constants,
+            .error_position = error_position,
+            .source = source,
+        };
+    }
+
+    pub fn deinit(self: *Parser) void {
+        self.memory.deinit();
+        free_keys_and_deinit(self.allocator, &self.labels);
+        free_keys_and_deinit(self.allocator, &self.constants);
+    }
+
+    pub fn run(self: *Parser) ![]i32 {
+        try self.collect_names();
+        try self.assemble();
+        return self.memory.toOwnedSlice();
+    }
+
+    fn collect_names(self: *Parser) !void {
+        var memory_address: i32 = 0;
+        var lines = std.mem.tokenizeScalar(u8, self.source, '\n');
+        var current_line: u32 = 0;
+        errdefer self.error_position.* = current_line;
+        while (lines.next()) |line| {
+            current_line +|= 1;
+            var words = std.mem.tokenizeAny(u8, line, " \t\r");
+            if (words.peek()) |word| {
+                switch (word[0]) {
+                    ':' => {
+                        const entry = try self.labels.getOrPut(word[1..]);
+                        if (entry.found_existing) return error.label_collision;
+                        entry.value_ptr.* = memory_address;
+                    },
+                    '>' => {
+                        const entry = try self.constants.getOrPut(word[1..]);
+                        if (entry.found_existing) return error.constant_collision;
+                        entry.value_ptr.* = try parse_line(line);
+                    },
+                    else => {},
+                }
+                memory_address +%= @bitCast(try line_size(line));
+            }
+        }
+    }
+
+    fn assemble(self: *Parser) !void {
+        _ = self; // autofix
+        unreachable;
+    }
+
+    fn free_keys_and_deinit(
+        allocator: std.mem.Allocator,
+        map: *Environment,
+    ) void {
+        var keys = map.keyIterator();
+        while (keys.next()) |key| {
+            allocator.free(key.*);
+        }
+        map.deinit();
+    }
+};
+
 
 pub const lemos_dialect = Dialect.initComptime(.{
     .{ "fetch", .pc_fetch },
@@ -60,20 +164,9 @@ const whitespace = std.StaticStringMap(void).initComptime(
     .{ .{" "}, .{"\t"}, .{"\n"}, .{"\r"} },
 );
 
-fn free_keys_and_deinit(
-    allocator: std.mem.Allocator,
-    map: *Environment,
-) void {
-    var keys = map.keyIterator();
-    while (keys.next()) |key| {
-        allocator.free(key.*);
-    }
-    map.deinit();
-}
-
 fn any_whitespace(subject: []const u8) []const u8 {
     return for (subject, 0..) |character, index| {
-        if (!whitespace.has(character))
+        if (!whitespace.has(&.{character}))
             break subject[index..];
     } else subject;
 }
@@ -88,7 +181,7 @@ fn one_word(subject: []const u8) ?With_Capture {
     return if (no_leading_whitespace.len == 0)
         null
     else for (no_leading_whitespace, 0..) |character, index| {
-        if (whitespace.has(character)) {
+        if (whitespace.has(&.{character})) {
             assert(index != 0);
             break .{
                 .capture = no_leading_whitespace[0 .. index - 1],
@@ -101,19 +194,25 @@ fn one_word(subject: []const u8) ?With_Capture {
     };
 }
 
-fn string_line_size(line: []const u8) !u32 {
-    return if (one_word(line)) |word| blk: {
-        const capture, const remainder = word;
-        break :blk switch (capture[0]) {
-            ':' => @call(.always_tail, string_line_size, .{remainder}),
+fn string_line_size_impl(comptime nested: bool, line: []const u8) !u32 {
+    if (one_word(line)) |word| {
+        return switch (word.capture[0]) {
+            ':' => if (nested)
+                error.invalid_line
+            else
+                string_line_size_impl(true, word.remainder),
             '"' => unreachable, // FIXME
             else => error.not_actually_a_string,
         };
-    } else error.empty_line;
+    } else return error.empty_line;
+}
+
+fn string_line_size(line: []const u8) !u32 {
+    return string_line_size_impl(false, line);
 }
 
 fn line_size(line: []const u8) !u32 {
-    var words = std.mem.tokenizeAny(line, " \t\r");
+    var words = std.mem.tokenizeAny(u8, line, " \t\r");
     if (words.peek() == null) return 0;
     return while (words.next()) |word| {
         break switch (word[0]) {
@@ -129,72 +228,6 @@ fn parse_line(_: []const u8) !i32 {
     unreachable; // FIXME
 }
 
-fn collect_names(
-    labels: *Environment,
-    constants: *Environment,
-    source: []const u8,
-    error_position: *u32,
-) !void {
-    var memory_address: i32 = 0;
-    var lines = std.mem.tokenizeScalar(u8, source, '\n');
-    var current_line: u32 = 0;
-    errdefer error_position.* = current_line;
-    while (lines.next()) |line| {
-        current_line +|= 1;
-        var words = std.mem.tokenizeAny(u8, line, " \t\r");
-        if (words.peek()) |word| {
-            switch (word[0]) {
-                ':' => {
-                    const entry = try labels.getOrPut(word[1..]);
-                    if (entry.found_existing) return error.label_collision;
-                    entry.value_ptr.* = memory_address;
-                },
-                '>' => {
-                    const entry = try constants.getOrPut(word[1..]);
-                    if (entry.found_existing) return error.constant_collision;
-                    entry.value_ptr.* = try parse_line(line);
-                },
-                else => {},
-            }
-            memory_address +%= try line_size(line);
-        }
-    }
-}
-
-fn assemble(
-    labels: *Environment,
-    constants: *Environment,
-    source: []const u8,
-    memory: *Memory,
-    error_position: *u32,
-) !void {
-    _ = error_position; // autofix
-    _ = memory; // autofix
-    _ = source; // autofix
-    _ = constants; // autofix
-    _ = labels; // autofix
-    unreachable;
-}
-
-pub fn parse(
-    self: Chaff,
-    source: []const u8,
-    error_position: *u32,
-) ![]i32 {
-    assert(source.len < std.math.maxInt(u32) + 2);
-    const allocator = self.allocator;
-
-    var memory = Memory.init(allocator);
-    errdefer memory.deinit();
-
-    var labels = Environment.init(allocator);
-    defer free_keys_and_deinit(allocator, &labels);
-
-    var constants = Environment.init(allocator);
-    defer free_keys_and_deinit(allocator, &constants);
-
-    try collect_names(&labels, &constants, source, error_position);
-    try assemble(&labels, &constants, source, &memory, error_position);
-
-    return memory.toOwnedSlice();
+test "everything" {
+    std.testing.refAllDecls(@This());
 }
