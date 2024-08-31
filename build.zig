0e3f1a17525591781992c652d62b9f122353c220
Marinho <marinho.pinotti@gmail.com>
2024-08-31 16:46:14 -0300

    Add chaff executable to `build.zig`

--- build.zig
+++ build.zig
@@ -5,29 +5,35 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const vm = b.addExecutable(.{
         .name = "elevator-chime",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
+    b.installArtifact(vm);
+    const run_cmd = b.addRunArtifact(vm);
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    const chaff = b.addExecutable(.{
+        .name = "chaff",
+        .root_source_file = b.path("src/chaff.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(chaff);
+    const compile_cmd = b.addRunArtifact(chaff);
+    compile_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        compile_cmd.addArgs(args);
+    }
+    const compile_step = b.step("chaff", "Compile a program written in chaff assembly syntax");
+    compile_step.dependOn(&compile_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
